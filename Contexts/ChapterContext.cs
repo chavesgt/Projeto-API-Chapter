5bbd0e1edbb78d7b1332f2e7e5ccf9b0abec39b0
Gabriel Chaves <teixeira.gabriel11@gmail.com>
2024-07-28 01:23:22 -0300

    Create API

---
+++ Contexts/ChapterContext.cs
@@ -0,0 +1,37 @@
+using Chapter.WebApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
+namespace Chapter.WebApi.Contexts
+{
+    public class ChapterContext : DbContext
+    {
+        public ChapterContext()
+        {
+        }
+        public ChapterContext(DbContextOptions<ChapterContext>
+        options) : base(options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder
+        optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                // Essa string de conexão foi depende da SUA máquina.
+                //optionsBuilder.UseSqlServer("Server=localhost\\SQLEXP
+                //RESS01; "
+                //+
+                //"Database=Chapter;Trusted_Connection=True;");
+                // Exemplo 1 de string de conexão:
+                //User ID=sa;Password=576171;Server=localhost;Database=Chapter;-
+                // + Trusted_Connection=False;
+                // Exemplo 2 de string de conexão:
+                // 
+                //Server = localhost\\SQLEXPRESS; Database = Chapter; Trusted_Connection = True
+                ;
+            }
+        }
+        public DbSet<Livro> Livros { get; set; }
+    }
+}
